c2a412b91a09b9633ce3a6bc82df674f113090e4
takabo <thousandbeach.td@gmail.com>
2018-03-01 14:41:11 +0000

    CRUD tasklists

---
+++ app/Http/Controllers/TasklistsController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Tasklist;   // 藤原 追加
+
+class TasklistsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     // getでtasklists/にアクセスされた場合の『一覧表示処理』
+    public function index()
+    {
+        $tasklists = Tasklist::all();
+
+        return view('tasklists.index', ['tasklists' => $tasklists, ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     // getでtasklists/createにアクセスされた場合の『新規登録画面表示処理』
+    public function create()
+    {
+        $tasklist = new Tasklist;
+
+        return view('tasklists.create', ['tasklist' => $tasklist, ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+     // postでtasklists/にアクセスされた場合の『新規登録処理』
+    public function store(Request $request)
+    {
+        $tasklist = new Tasklist;
+        $tasklist->content = $request->content;
+        $tasklist->save();
+
+        return redirect('/'); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     // getでtasklists/idにアクセスされた場合の『取得表示処理』
+    public function show($id)
+    {
+        $tasklist = Tasklist::find($id);
+        return view('tasklists.show', ['tasklist' => $tasklist]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     // getでtasklists/id/editにアクセスされた場合の『更新画面表示処理』
+    public function edit($id)
+    {
+        $tasklist = Tasklist::find($id);
+        return view('tasklists.edit', ['tasklist' => $tasklist,]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     // putまたはpatchでtasklists/idにアクセスされた場合の『更新処理』
+    public function update(Request $request, $id)
+    {
+        $tasklist = Tasklist::find($id);
+        $tasklist->content = $request->content;
+        $tasklist->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     // deleteでtasklists/idにアクセスされた場合の『削除処理』
+    public function destroy($id)
+    {
+        $tasklist = Tasklist::find($id);
+        $tasklist->delete();
+
+        return redirect('/');
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,37 @@
 |
 */
 
+/*
+// CRUD
+// タスクリストの個別詳細ページ表示
+Route::get('tasklists/{id}', 'TasklistsController@show');
+// タスクリストの新規登録を処理（新規登録画面を表示するためのものではない）
+Route::post('tasklists', 'TasklistsController@store');
+// タスクリストの更新処理（編集画面を表示するためのものではない）
+Route::put('tasklists/{id}', 'TasklistsController@update');
+// タスクリストを削除
+Route::delete('tasklists/{id}', 'TasklistsController@destroy');
+
+// index: showの補助ページ
+Route::get('tasklists', 'TasklistsController@index');
+
+// create: 新規作成用のフォームページ
+Route::get('tasklists/create', 'TasklistsController@create');
+
+// edit: 更新用のフォームページ
+Route::get('tasklists/{id}/edit', 'TasklistsController@edit');
+
+
+// f複写機能（ボタン）
+Route::get('tasklists/{id}/copy', 'TasklistsController@copy');
+*/
+
+/* デフォルトの状態
 Route::get('/', function () {
     return view('welcome');
 });
+*/ 
+
+
+Route::get('/', 'TasklistsController@index');
+Route::resource('tasklists', 'TasklistsController');
