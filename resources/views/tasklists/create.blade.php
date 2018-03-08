3ebd4ac1529685fa6067d99d665e6aee08a1c1a1
takabo <thousandbeach.td@gmail.com>
2018-03-08 04:55:59 +0000

    bootstrap design_4

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -3,34 +3,32 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
-    <div class="row"><!-- START row -->
-        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
-            <h1>タスク新規作成ページ</h1>
-        </div>
-    </div><!-- END row -->
-    
-    {!! Form::model('tasklist', ['route' => 'tasklists.store']) !!}
 
-        <div class="row"><!-- START row -->
-            
-                <div class="form-group col-xs-12 col-sm-4 col-md-4 col-lg-3">
-                    {!! Form::label('status', 'ステータス') !!}
-                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
-                </div>
-            
-            
-                <div class="form-group col-xs-12 col-sm-offset-2 col-sm-4 col-md-offset-2 col-md-4 col-lg-offset-3 col-lg-3">
-                    {!! Form::label('content', 'タスク') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                </div>
-            
-        </div><!-- END row -->
-        <div class="row"><!-- START row -->
-            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
-            {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+    <div class="row"><!-- START row -->
+
+        <div class="form-group col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
+            <h1>タスク新規作成ページ</h1>
+
+        {!! Form::model('tasklist', ['route' => 'tasklists.store']) !!}
+
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
             </div>
-        </div><!-- END row -->
-        
-    {!! Form::close() !!}
+
+            <div class="form-group">
+                {!! Form::label('content', 'タスク') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+
+                {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+
+        {!! Form::close() !!}
+
+        </div><!-- END form-group -->
+
+    </div><!-- END row -->
+
 
 @endsection
--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -3,18 +3,30 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
+<div class="row">
+    
+    <div class="form-group col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
     <h1>id： {{ $tasklist->id }} のタスク編集ページ</h1>
 
     {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put'] ) !!}
 
+        <div class="form-group">
         {!! Form::label('status', 'ステータス：') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
 
+        <div class="form-group">
         {!! Form::label('content', 'タスク：') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
 
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
+
+    </div>
+
+</div>
 
 @endsection
